82aa06dffb48c4592b627fac6c14a88da6a798ae
falahh6 <falahsss900@gmail.com>
2025-01-19 22:16:43 +0530

    ws configs

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,10 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
+    const isProd = process.env.NODE_ENV === "production";
+    console.log("NODE ENV : ", process.env.NODE_ENV);
     return [
       {
         source: "/ws",
-        destination: `http://localhost:${process.env.NEXT_PUBLIC_WS_PORT}/ws`,
+        destination: isProd
+          ? `https://${process.env.NEXT_PUBLIC_WS_DOMAIN}/ws` // Use the production WebSocket URL (wss://)
+          : `http://localhost:${process.env.NEXT_PUBLIC_WS_PORT}/ws`, // Use local WebSocket for development
       },
     ];
   },
